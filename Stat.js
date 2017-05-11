aca6147b30be00fee263f23600870e9ba1351674
BangDidie <bangdidie1@gmail.com>
2017-05-11 17:41:35 +0700

    Add files via upload

---
+++ Stat.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+//<![CDATA[
+var sc_project=11340647; 
+var sc_invisible=0; 
+var sc_security="a61d137b"; 
+var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+ "statcounter.com/counter/counter_xhtml.js'></"+"script>");
+//]]>
+</script>
